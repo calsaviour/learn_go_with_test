41276f03f1da301e599b963b0e4012d5fa74930f
Calvin Low <calsaviour@gmail.com>
2021-03-27 15:19:21 +0800

    Add spanish

--- hello_test.go
+++ hello_test.go
@@ -23,4 +23,9 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
